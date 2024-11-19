2b368fbdac4e35a03352fd4ea0a6046f84e37802
Uladzislau <yanushevskyv@gmail.com>
2024-11-19 23:23:27 +0100

    feat : added docker files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoriteModule } from './favorite/favorite.module';
 import { DbModule } from 'src/db/db.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -14,6 +15,16 @@ import { DbModule } from 'src/db/db.module';
     ArtistModule,
     AlbumModule,
     DbModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      database: process.env.POSTGRES_DB_NAME,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [],
   providers: [],
